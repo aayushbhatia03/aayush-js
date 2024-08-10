da1575cd11721670316aaf5b9d6e15d498c335d7
aayush <153361739+aayushbhatia03@users.noreply.github.com>
2024-08-10 15:17:13 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountid = 144553
+let email = "ab24@abc.com"
+var accountpassword = 3455
+accountcity = "sirsa"
+let accountstate;
+/*
+do not use var as it don not understand scope 
+if we do not give a value it gives undifned
+*/
+
+email = "gif@Gd.com"
+accountpassword = 6788
+accountcity = "mohali"
+// accountid = 2345 // not allowed
+
+console.table([accountstate , accountid ,email , accountpassword , accountcity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; //to treat all code as new version of code
+let age = 17
+let name = "aayush"
+console.log(typeof "aayush")
+console.log(typeof null)
+console.log(typeof undefined)
+// null is object 
